fb9c2e59caf38dfc418d51b9139a33d57b60014e
Ramon Tanoeiro <ramontanoeiro@gmail.com>
2023-12-16 12:00:29 +1100

    Adjusted dbt model

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,1 +1,14 @@
-select * from dev.stripe.payment
+with payments as (
+
+    select
+        payment.id,
+        payment.orderid,
+        payment.paymentmethod,
+        payment.status,
+        payment.amount,
+        payment.created,
+        payment._batched_at
+    from dev.stripe.payment
+)
+
+select * from payments

bcbf7a25df946db22445c599830b86c2deab7412
Ramon Tanoeiro <ramon.tanoeiro@indebted.co>
2023-12-28 19:23:48 +1100

    added assert for valid customers on orders table

---
+++ tests/assert_stg_orders_customer_id_is_valid.sql
@@ -0,0 +1,11 @@
+with customer as (
+    select * from ref('stg_customers')
+),
+
+orders as (
+    select * from ref('stg_orders')
+),
+
+select distinct customer.customer_id
+from customer
+where customer.customer_id not in (select orders.customer_id from orders)
